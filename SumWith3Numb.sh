18930cb3232a03d19f401bdf3d9c26f7668910f6
Rafael Ribeiro <54875451+Ycatam@users.noreply.github.com>
2020-04-23 16:05:06 -0300

    Create SumWith3Numb.sh

---
+++ SumWith3Numb.sh
@@ -0,0 +1,11 @@
+#!usr/bin/env bash
+echo "$0"
+
+echo "Type the first number: "
+read numb1
+echo "Type the second number: "
+read numb2
+echo "Type the third number: "
+read numb3
+sum1=$(( numb1 + numb2 + numb3 ))
+echo "The sum of $numb1, $numb2 and $numb3 is $sum1."
